24131d91e1414646a98b9f9784148add2da5e3ad
Ivan <8102529@gmail.com>
2020-02-26 16:15:56 +0300

    отработка вывода данных из бд на главную страницу

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Genre, Movie, MovieShots, Reviews, Rating, RatingStar, Actor
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieShots)
+admin.site.register(Reviews)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
+admin.site.register(Actor)
